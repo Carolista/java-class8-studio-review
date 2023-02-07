8b79715735af4969a76a2605d7aea718932808f6
Carolista <cjones.renaissancewoman@gmail.com>
2023-02-06 21:53:30 -0600

    simplified import statements

--- class8-studio/src/main/java/org/launchcode/class8studio/controllers/SkillsController.java
+++ class8-studio/src/main/java/org/launchcode/class8studio/controllers/SkillsController.java
@@ -1,10 +1,7 @@
 package org.launchcode.class8studio.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
